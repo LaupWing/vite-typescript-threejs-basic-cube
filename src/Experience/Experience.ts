02727ec08eba8768bac2d20ada5700d24788144c
LaupWing <loc_strict@hotmail.com>
2023-06-27 12:12:55 +0200

    added camera

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,14 +1,17 @@
 import * as THREE from "three"
 import Sizes from "./utils/Sizes"
+import Camera from "./Camera"
 
 export default class Experience {
    public canvas: HTMLCanvasElement
    public scene: THREE.Scene
    public sizes: Sizes
+   public camera: Camera
 
    constructor (canvas: HTMLCanvasElement) {
       this.canvas = canvas
       this.scene = new THREE.Scene()
       this.sizes = new Sizes()
+      this.camera = new Camera(this)
    }
 }
