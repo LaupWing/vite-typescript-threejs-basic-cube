9fb75518181c797f5d7ede5ac50adfb1734aa43c
LaupWing <loc_strict@hotmail.com>
2023-06-27 12:11:55 +0200

    udpate

---
+++ src/Experience/Renderer.ts
@@ -0,0 +1,10 @@
+import * as THREE from "three"
+import Experience from "./Experience"
+
+export default class Renderer {
+   private experience: Experience
+   private instance: THREE.WebGLRenderer
+   private scene: Experience["scene"]
+   private sizes: Experience["sizes"]
+   // private camera: Experience[""]
+}
