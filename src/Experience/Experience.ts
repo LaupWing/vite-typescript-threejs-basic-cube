ed78e4e7b279467e866ce4ea419c97e4c220a833
LaupWing <loc_strict@hotmail.com>
2023-06-27 12:18:35 +0200

    add

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,17 +1,21 @@
 import * as THREE from "three"
 import Sizes from "./utils/Sizes"
 import Camera from "./Camera"
+import BasicCube from "./BasicCube"
 
 export default class Experience {
    public canvas: HTMLCanvasElement
    public scene: THREE.Scene
    public sizes: Sizes
    public camera: Camera
+   public basicCube: BasicCube
 
    constructor (canvas: HTMLCanvasElement) {
       this.canvas = canvas
       this.scene = new THREE.Scene()
       this.sizes = new Sizes()
       this.camera = new Camera(this)
+      this.basicCube = new BasicCube()
+      this.scene.add(this.basicCube.instance)
    }
 }
