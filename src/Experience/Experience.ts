462e5420d9d0300cb07d3397b1403901ea68c700
LaupWing <loc_strict@hotmail.com>
2023-06-27 11:49:31 +0200

    three scene

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,1 +1,11 @@
 import * as THREE from "three"
+
+export default class Experience {
+   public canvas: HTMLCanvasElement
+   public scene: THREE.Scene
+
+   constructor (canvas: HTMLCanvasElement) {
+      this.canvas = canvas
+      this.scene = new THREE.Scene()
+   }
+}
