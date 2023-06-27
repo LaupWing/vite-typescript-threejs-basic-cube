3b533774d01facd1cb35bc400d13056acf865bea
LaupWing <loc_strict@hotmail.com>
2023-06-27 12:03:58 +0200

    udpate

--- src/Experience/Camera.ts
+++ src/Experience/Camera.ts
@@ -8,11 +8,20 @@ export default class Camera {
    private sizes: Experience["sizes"]
    private scene: Experience["scene"]
    private canvas: Experience["canvas"]
+   public instance: THREE.PerspectiveCamera
 
    constructor(experience: Experience) {
       this.experience = experience
       this.sizes = this.experience.sizes
       this.scene = this.experience.scene
       this.canvas = this.experience.canvas
+      this.instance = new THREE.PerspectiveCamera(
+         75,
+         this.sizes.width / this.sizes.height,
+         0.1,
+         100
+      )
+      this.instance.position.set(1, 1, 1)
+      this.scene.add(this.instance)
    }
 }
