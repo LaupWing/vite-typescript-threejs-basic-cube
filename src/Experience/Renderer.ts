b5983860579b55b1cd342faee4ea42b3ffc8be58
LaupWing <loc_strict@hotmail.com>
2023-06-27 12:15:56 +0200

    duapte

--- src/Experience/Renderer.ts
+++ src/Experience/Renderer.ts
@@ -6,5 +6,22 @@ export default class Renderer {
    private instance: THREE.WebGLRenderer
    private scene: Experience["scene"]
    private sizes: Experience["sizes"]
-   // private camera: Experience[""]
+   private camera: Experience["camera"]
+   private canvas: Experience["canvas"]
+   
+   constructor(experience: Experience){
+      this.experience = experience
+      this.canvas = this.experience.canvas
+      this.scene = this.experience.scene
+      this.camera = this.experience.camera
+      this.sizes = this.experience.sizes
+      this.instance = new THREE.WebGLRenderer({
+         canvas: this.canvas,
+         antialias: true,
+         alpha: true
+      })
+
+      this.instance.setSize(this.sizes.width, this.sizes.height)
+      this.instance.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+   }
 }
