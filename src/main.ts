268cb186dba979e36a50cf5f654f047f80e4741e
LaupWing <loc_strict@hotmail.com>
2023-07-12 19:16:11 +0200

    removed logs

--- src/main.ts
+++ src/main.ts
@@ -8,43 +8,31 @@ import {
    WebGLRenderer,
 } from "three"
 
-// Get a reference to the container element that will hold our scene
 const container = document.querySelector(".webgl")!
 
-// create a Scene
 const scene = new Scene()
 
-// Set the background color
 scene.background = new Color("skyblue")
 
-// Create a camera
-const fov = 35 // AKA Field of View
+const fov = 35
 const aspect = container.clientWidth / container.clientHeight
-const near = 0.1 // the near clipping plane
-const far = 100 // the far clipping plane
+const near = 0.1
+const far = 100 
 
 const camera = new PerspectiveCamera(fov, aspect, near, far)
 
-// every object is initially created at ( 0, 0, 0 )
-// move the camera back so we can view the scene
 camera.position.set(0, 0, 10)
 
-// create a geometry
 const geometry = new BoxGeometry(2, 2, 2)
 
-// create a default (white) Basic material
 const material = new MeshBasicMaterial()
 
-// create a Mesh containing the geometry and material
 const cube = new Mesh(geometry, material)
 
-// add the mesh to the scene
 scene.add(cube)
 
-// create the renderer
 const renderer = new WebGLRenderer()
 
-// next, set the renderer to the same size as our container element
 renderer.setSize(container.clientWidth, container.clientHeight)
 
 renderer.setPixelRatio(window.devicePixelRatio)
