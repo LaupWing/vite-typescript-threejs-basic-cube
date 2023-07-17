01361ee31b691983025395bd484c637ce867d4d1
LaupWing <loc_strict@hotmail.com>
2023-07-17 17:15:43 +0200

    disabled the boilerplate code

--- src/main.ts
+++ src/main.ts
@@ -11,33 +11,33 @@ import {
 
 const container = document.querySelector(".webgl")!
 
-const scene = new Scene()
+// const scene = new Scene()
 
-scene.background = new Color("skyblue")
+// scene.background = new Color("skyblue")
 
-const fov = 35
-const aspect = container.clientWidth / container.clientHeight
-const near = 0.1
-const far = 100 
+// const fov = 35
+// const aspect = container.clientWidth / container.clientHeight
+// const near = 0.1
+// const far = 100 
 
-const camera = new PerspectiveCamera(fov, aspect, near, far)
+// const camera = new PerspectiveCamera(fov, aspect, near, far)
 
-camera.position.set(1, 1, 10)
+// camera.position.set(1, 1, 10)
 
-const geometry = new BoxGeometry(2, 2, 2)
+// const geometry = new BoxGeometry(2, 2, 2)
 
-const material = new MeshBasicMaterial()
+// const material = new MeshBasicMaterial()
 
-const cube = new Mesh(geometry, material)
+// const cube = new Mesh(geometry, material)
 
-scene.add(cube)
+// scene.add(cube)
 
-const renderer = new WebGLRenderer({
-   canvas: container
-})
+// const renderer = new WebGLRenderer({
+//    canvas: container
+// })
 
-renderer.setSize(container.clientWidth, container.clientHeight)
+// renderer.setSize(container.clientWidth, container.clientHeight)
 
-renderer.setPixelRatio(window.devicePixelRatio)
+// renderer.setPixelRatio(window.devicePixelRatio)
 
-renderer.render(scene, camera)
+// renderer.render(scene, camera)
