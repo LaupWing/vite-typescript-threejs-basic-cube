3cdf276b378d734d4e09bb6a5aba0254c5c11005
LaupWing <loc_strict@hotmail.com>
2023-07-18 17:31:10 +0200

    added the globals

--- src/Experience/utils/Time.ts
+++ src/Experience/utils/Time.ts
@@ -2,6 +2,12 @@ import * as THREE from "three"
 import { EventEmitter } from "./EventEmitter"
 
 export default class Time extends EventEmitter {
+    private start: number
+    private current: number
+    public elapsed: number
+    public delta: number
+    public clock: THREE.Clock
+
     constructor() {
 
     }
