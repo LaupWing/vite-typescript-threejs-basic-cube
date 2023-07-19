cdc6ad9c9ea7db2c065ec99c5e1cdcf59136c58b
LaupWing <loc_strict@hotmail.com>
2023-07-19 17:17:51 +0200

    udpated

--- src/Experience/Configuration.ts
+++ src/Experience/Configuration.ts
@@ -1,8 +1,11 @@
 export default class Configuration {
     public color: number
+    private color_element: HTMLInputElement
 
     constructor() {
-        this.color = 0xffccff
+        this.color_element = document.querySelector("#color") as HTMLInputElement
+        this.color = Number(this.color_element.value)
+        console.log(this.color_element.value)
     }
 
     setter(key: keyof this, value: any){
