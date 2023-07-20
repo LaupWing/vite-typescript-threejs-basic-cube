3d35ad9473d02f430f2789305bb593fe60eafd2f
LaupWing <loc_strict@hotmail.com>
2023-07-20 17:19:56 +0200

    added basic cube to pipleine

--- src/Experience/Configuration.ts
+++ src/Experience/Configuration.ts
@@ -1,10 +1,15 @@
+import BasicCube from "./BasicCube"
+import Experience from "./Experience"
+
 export default class Configuration {
     public color: number
     private color_element: HTMLInputElement
+    private basicCube: BasicCube
 
-    constructor() {
+    constructor(experience: Experience) {
         this.color_element = document.querySelector("#color") as HTMLInputElement
         this.color = Number(this.color_element.value)
+        this.basicCube = experience.basicCube
         this.attachEvents()
     }
 
