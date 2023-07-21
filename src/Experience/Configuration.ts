0123aba0dc4d6df605c1aafbe527dbac0318c083
LaupWing <loc_strict@hotmail.com>
2023-07-21 08:55:41 +0200

    updatd

--- src/Experience/Configuration.ts
+++ src/Experience/Configuration.ts
@@ -4,12 +4,12 @@ import Experience from "./Experience"
 export default class Configuration {
     public color: number
     private color_element: HTMLInputElement
-    private zAs_element: HTMLInputElement
+    private z_axis_element: HTMLInputElement
     private basicCube: BasicCube
 
     constructor(experience: Experience) {
         this.color_element = document.querySelector("#color") as HTMLInputElement
-        this.zAs_element = document.querySelector("#zAs") as HTMLInputElement
+        this.z_axis_element = document.querySelector("#zAxis") as HTMLInputElement
 
         this.color = Number(this.color_element.value)
         this.basicCube = experience.basicCube
@@ -19,12 +19,12 @@ export default class Configuration {
     attachEvents()
     {
         this.color_element.addEventListener("change", this.updateColor.bind(this))
-        this.zAs_element.addEventListener("change", this.updateSize.bind(this))
+        this.z_axis_element.addEventListener("change", this.updateSize.bind(this))
     }
 
     updateSize()
     {
-        const width = Number(this.zAs_element.value)
+        const z_axis = Number(this.z_axis_element.value)
         this.basicCube.instance.scale.z = 2
     }
 
